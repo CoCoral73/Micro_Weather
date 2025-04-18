97025fef72c430fd16cd0d6abc889c27c96a9b5a
CoCoral73 <garden0073@gmail.com>
2025-04-18 16:44:09 +0900

    create weather api manager

---
+++ MicroWeather/WeatherAPIManager.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherAPIManager.swift
+//  MicroWeather
+//
+//  Created by 김정원 on 4/18/25.
+//
+
+import Foundation
+
+final class WeatherAPIManager {
+    static let shared = WeatherAPIManager()
+    private init() {}
+    
+    
+}
