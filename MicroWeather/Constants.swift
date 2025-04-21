fb66c649fad4b4326f3e5ad071b24cecef2687dd
CoCoral73 <garden0073@gmail.com>
2025-04-21 16:26:20 +0900

    update spelling

--- MicroWeather/Constants.swift
+++ MicroWeather/Constants.swift
@@ -8,33 +8,33 @@
 import UIKit
 
 public enum WeatherAPIType {
-    case ultraSrtNst
-    case ultraSrtFst
-    case vilageFst
-    case fstVersion
+    case ultraSrtNcst
+    case ultraSrtFcst
+    case vilageFcst
+    case fcstVersion
     
     var endpoint: String {
         switch self {
-        case .ultraSrtNst:
-            return "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
-        case .ultraSrtFst:
-            return "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"
-        case .vilageFst:
-            return "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
-        case .fstVersion:
-            return "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getFcstVersion"
+        case .ultraSrtNcst:
+            return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
+        case .ultraSrtFcst:
+            return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"
+        case .vilageFcst:
+            return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+        case .fcstVersion:
+            return "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getFcstVersion"
         }
     }
     
     var modelType: Decodable.Type {
         switch self {
-        case .ultraSrtNst:
+        case .ultraSrtNcst:
             return UltraShortTermObservations.self
-        case .ultraSrtFst:
+        case .ultraSrtFcst:
             return UltraShortTermForecast.self
-        case .vilageFst:
+        case .vilageFcst:
             return ShortTermForecast.self
-        case .fstVersion:
+        case .fcstVersion:
             return ForecastVersion.self
         }
     }
