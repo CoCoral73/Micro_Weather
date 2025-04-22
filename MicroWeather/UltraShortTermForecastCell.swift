a595ea4e1429f54986b0b7fd36664a25ef2a7e10
CoCoral73 <garden0073@gmail.com>
2025-04-22 15:56:47 +0900

    Create UltraShortTermForecastCell.swift

---
+++ MicroWeather/UltraShortTermForecastCell.swift
@@ -0,0 +1,21 @@
+//
+//  UltraShortTermForecastCell.swift
+//  MicroWeather
+//
+//  Created by 김정원 on 4/22/25.
+//
+
+import UIKit
+
+class UltraShortTermForecastCell: UITableViewCell {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var iconView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
