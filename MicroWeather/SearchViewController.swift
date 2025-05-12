adc9fc1782b0b9674256a942fc131bbaab46013a
CoCoral73 <garden0073@gmail.com>
2025-05-12 13:51:58 +0900

    Create SearchViewController.swift

---
+++ MicroWeather/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  MicroWeather
+//
+//  Created by 김정원 on 5/12/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
